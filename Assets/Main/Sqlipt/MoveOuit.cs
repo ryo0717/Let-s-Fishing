fa49ddd1ec808713c570d18e78c6b8f36a0ca264
furusho3116 <kdg.ryo.furusho@gmail.com>
2020-06-08 10:53:48 +0900

    fourth

---
+++ Assets/Main/Sqlipt/MoveOuit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveOuit : MonoBehaviour
+{
+    [SerializeField] 
+    private SoundManager soundmanager;
+    public void OnClick(){
+        soundmanager.Button();
+        
+        #if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#elif UNITY_WEBPLAYER
+		Application.OpenURL("http://www.yahoo.co.jp/");
+		#else
+		Application.Quit();
+		#endif
+    }
+}
